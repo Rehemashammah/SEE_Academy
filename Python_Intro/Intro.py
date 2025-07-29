6c2de5713f115205cfe1339cf0ab0cd1859b1cb3
Rehemashammah <rehemashammah@gmail.com>
2025-07-29 19:51:02 +0300

    Intoduction to python

---
+++ Python_Intro/Intro.py
@@ -0,0 +1,32 @@
+# PLP = "Academy"
+# Student_Name = "Abdi Kadir"
+# Student_ID = 1001
+# Student_Age = 20
+# Fees_Clearance = True 
+
+# #Output
+# print(Student_Name, Student_ID, Student_Age, Fees_Clearance)
+
+#Lists - use square brackets
+# languages = [1995, "Python", "Java", "C++", "JavaScript"]
+# print(languages)
+
+# shopping_list = ["Milk", "Eggs", "Bread", "Butter"]
+# print(shopping_list[3])
+
+# #Tuples - use parenthesis
+# products = ('XBox', 499.99, "Habibi", 23)
+# print(products)
+
+# #Sets - use curly braces
+# Set_A = {1, 2, 3, 4, 5}
+# Set_B = {4, 5, 6, 7, 8}
+# print(Set_A)
+# print(Set_B)
+
+#Dictionary
+Student_Info = {"Name" : "Abdi Kadir", "ID" : 1001, "Age": 20, "Fee_Clearance": True}
+print(Student_Info)
+
+capital_city = {"Kenya" : "Nairobi", "Nigeria" : "Lagos", "Uganda" : "Kampala"}
+print(capital_city)
